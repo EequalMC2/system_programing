fcf63d86ee15501cb9eac9e2839c8cc3db652c10
EequalMC2 <muratonr@yahoo.com.tr>
2019-02-07 18:04:50 +0300

    deleted getopt file

--- system_programing/main.c
+++ system_programing/main.c
@@ -1,7 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
+
+int main (int argc, const char* argv[] )
 {
-    printf("Hello World!\n");
-    return 0;
-}
+    int result;
+    int aflag = 0, bflag = 0, cflag = 0, dflag = 0;
+    char *carg, *darg;
+    int i;
+
+    opterr = 0;
+    while((result = getopt(argc, argv, "abc:d:")) != -1)
+    {
+        switch(result){
+
+        case 'a' :
+            aflag = 1;
+            break;
+
+        case 'b' :
+            bflag = 1;
+            break;
+        case 'c' :
+            cflag = 1;
+            carg = optarg;
+            break;
+
+        case 'd' :
+            dflag = 1;
+            darg = optarg;
+
+        case '?' :
+            if(optopt == 'c' || optopt == 'd')
+                fprintf(stderr,"-%c option  given without an arguman\n",optopt);
+
+            else
+                fprintf(stderr, "invalid switch %c \n",optopt);
+            break;
+        }
+
+        fprintf(stdout,"arguman without switch\n");
+        for(int i = optind; i < argc; ++i)
+            fputs(argv[i],stdout);
+
+        if(aflag)
+            puts("-a is given\n");
+
+        if(bflag)
+            puts("-b is given\n");
+
+        if(cflag)
+            printf("-c is given, its argument: %s \n", carg);
+
+        if(dflag)
+            printf("-d is given, its arguşment %s \n", darg);
+
+
+        return 0;
+        }
+
+    }
+
+
+
+
+
+
